08ca9be27635d898bcc6a29ab6b98fc328772a85
Dhruv <dhruv@cepheiinfotech.com>
2025-02-12 10:22:52 +0530

    update pdf 12-02-2025

--- src/Components/Gliptozapdf.jsx
+++ src/Components/Gliptozapdf.jsx
@@ -18,25 +18,25 @@
 import React from "react";
 
 const PdfViewer2 = () => {
-  const pdfPath = "/assets/GLIPTOZA PATIENT SUPPORT.pdf"; // Correct way to reference a file in `public/`
+  const pdfPath = "/assets/LYRATO BOOKLET_1A (1).pdf"; // Correct way to reference a file in `public/`
 
   return (
     <div style={{ margin: 0, padding: 0, height: "100vh", overflow: "hidden", display: "flex", flexDirection: "column" }}>
       {/* Embedded PDF Viewer */}
-      <h1>Comming soon</h1>
-      {/* <embed
+      {/* <h1>Comming soon</h1> */}
+      <embed
         src={pdfPath}
         style={{ width: "100%", height: "100vh", border: "none", position: "absolute" }}
         title="PDF Viewer"
         type="application/pdf"
-      /> */}
+      />
 
       {/* Fallback: Button to Open in a New Tab */}
-      {/* <div style={{ position: "absolute", top: 10, right: 10 }}>
+      <div style={{ position: "absolute", top: 10, right: 10 }}>
         <button onClick={() => window.open(pdfPath, "_blank")} style={{ padding: "10px", fontSize: "16px" }}>
           Open PDF in New Tab
         </button>
-      </div> */}
+      </div>
     </div>
   );
 };
